b991ec1546674a94b20685620c15677b293d2224
daniel <danielhsj@sina.cn>
2017-08-31 16:27:56 +0800

    完成首页

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+$(function () {
+
+
+    /*获取当前所有item*/
+    var items = $(".rightbox .item");
+    getPhoto(items);
+    var jiaJuPhoto = $('#jiaJuPhoto .item');
+    getPhoto(jiaJuPhoto);
+    var kuang = $('.kuang .item');
+    getPhoto(kuang);
+
+    function getPhoto(items) {
+        /*监听屏幕的大小改变*/
+        $(window).on("resize", function () {
+            /*1.获取当前屏幕的宽度*/
+            var width = $(window).width();
+            /*2.判断屏幕的宽度*/
+            if (width >= 768) {/*说明非移动端*/
+                /*为每一个item添加子元素--遍历*/
+                $(items).each(function (index, value) {
+                    var item = $(this);
+                    /*当前自定义属性中 存储的图片路径*/
+                    var imgSrc = item.data("largeImage");
+                    /*添加非移动端的子元素*/
+                    item.html($('<a href="javascript:;" class="pcImg"></a>').css("backgroundImage", "url('" + imgSrc + "')"));
+                });
+            }
+            else {
+                $(items).each(function (index, value) {
+                    var item = $(this);
+                    var imgSrc = item.data("smallImage");
+                    item.html('<a href="javascript:;" class="mobileImg"><img src="' + imgSrc + '" alt="..."></a>');
+                });
+            }
+        }).trigger("resize");
+    }
+
+
+});
